6e60415c116a4141d6cb136f3b301250338ce7e4
Cathrine Søby Rathje <csra@itu.dk>
2019-02-27 15:34:53 +0100

    added generate button

--- src/app/pages/sights/sights.page.ts
+++ src/app/pages/sights/sights.page.ts
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sights.page.scss'],
 })
 export class SightsPage implements OnInit {
+  sightChecked: boolean;
+  numberOfSightsChecked: number;
 
-  constructor() { }
+  constructor() { 
+    this.sightChecked = false;
+    this.numberOfSightsChecked = 0;
+  }
 
   ngOnInit() {
+  }
+
+  checkSight(event) {
+    if (event.target.checked) {
+      this.numberOfSightsChecked--;
+    } else {
+      this.numberOfSightsChecked++;
+    }
+    if (this.numberOfSightsChecked === 0) {
+      this.sightChecked = false;
+    } else {
+      this.sightChecked = true;
+    }
   }
 
 }
